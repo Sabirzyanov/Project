0015c165d56de090b6664cf5a25e806e3dac7c27
Sabirzyanov <sabirzyanov439@yandex.ru>
2020-01-17 17:11:49 +0300

    Version 1.0. Realise

--- PyGame2077.py
+++ PyGame2077.py
@@ -83,7 +83,7 @@ anim_stay = [('%s/mario/0.png' % ICON_DIR, 0.1)]
 
 
 # Print Function
-def print_text(message, x, y, font_color=(0, 0, 0), font_type='PingPong.ttf', font_size=30):
+def print_text(message, x, y, font_color=(255, 255, 255), font_type='PingPong.ttf', font_size=30):
     font_type = pygame.font.Font(font_type, font_size)
     text = font_type.render(message, True, font_color)
     display.blit(text, (x, y))
@@ -408,6 +408,7 @@ def start_screen():
         text_coord += intro_rect.height
         display.blit(string_rendered, intro_rect)
     while True:
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
